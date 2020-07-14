dc8de3f30114ae5b260bd8272b2fe5217c2e7e7a
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2020-07-14 20:15:49 +0600

    Add files via upload

---
+++ Factors of a Positive Integer.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num,i;
+
+    cout<<"Enter a positive integer: ";
+    cin>>num;
+
+    cout<<"Factors of "<<num<<" are: ";
+
+    for(i=1;i<=num;i++)
+    {
+        if(num%i==0)
+        {
+            cout<<i<<" ";
+        }
+    }
+}
