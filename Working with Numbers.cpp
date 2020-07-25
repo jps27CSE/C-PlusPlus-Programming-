ba0279d3b5a9a8d2c3a1f954cbd19e8cf14c1f2a
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2020-07-25 14:11:58 +0600

    Add files via upload

---
+++ Working with Numbers.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    cout<< pow(2,2)<<endl;
+
+    cout<<sqrt(36)<<endl;
+
+    cout<<round(4.3)<<endl;
+
+    cout<<ceil(4.1)<<endl;
+
+    cout<<floor(4.8)<<endl;
+
+    cout<<fmax(1,5)<<endl;
+
+    cout<<fmin(1,5)<<endl;
+}
---
+++ Working with Strings.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name="I am Jack";
+
+    cout<<name.length()<<endl;
+
+    cout<<name.find("Jack",0)<<endl;
+
+    cout<<name.substr(5,4);
+
+
+}
